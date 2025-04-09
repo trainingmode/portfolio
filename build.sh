e9e6a81a8a993c1a7aa3354f35e756e76ed2d59f
trainingmode <fred.r.duarte@outlook.com>
2025-04-08 20:46:24 -0700

    Initial commit

--- build.sh
+++ build.sh
@@ -15,55 +15,55 @@ INPUT_DIR="${1:-markdown}"
 OUTPUT_DIR="${2:-projects}"
 
 if [ ! -d "$INPUT_DIR" ]; then
-  echo "🤔 Error: Input directory '$INPUT_DIR' does not exist."
+  echo "ERROR: Input directory '$INPUT_DIR' does not exist."
   exit 1
 fi
 
 HTML_LAYOUT_FILE="${4:-templates/layout.frag.html}"
 
 if [ ! -f "$HTML_LAYOUT_FILE" ]; then
-  echo "🤔 Error: Layout template '$HTML_LAYOUT_FILE' does not exist."
+  echo "ERROR: Layout template '$HTML_LAYOUT_FILE' does not exist."
   exit 1
 fi
 
 HTML_LAYOUT=$(<"$HTML_LAYOUT_FILE")
 
 if [ -z "$HTML_LAYOUT" ]; then
-  echo "🤔 Error: Layout template is empty."
+  echo "ERROR: Layout template is empty."
   exit 1
 fi
 
 mkdir -p "$OUTPUT_DIR"
 
 find "$INPUT_DIR" -name "*.md" | while read -r filepath; do
-  rel_path="${filepath#$INPUT_DIR/}"
-  out_path="$OUTPUT_DIR/${rel_path%.md}.html"
-  out_dir=$(dirname "$out_path")
-  title=$(basename "${rel_path%.md}")
+  path_relative="${filepath#$INPUT_DIR/}"
+  output_path="$OUTPUT_DIR/${path_relative%.md}.html"
+  output_directory=$(dirname "$output_path")
+  title=$(basename "${path_relative%.md}")
 
   # Encode path as URL-safe string (strip newlines to avoid trailing %0A)
-  url="$(dirname "$rel_path")/$(basename "${rel_path%.md}" | tr -d '\n' | jq -sRr @uri).html"
+  url="$(dirname "$path_relative")/$(basename "${path_relative%.md}" | tr -d '\n' | jq -sRr @uri).html"
   # Extract first non-empty line, remove quotes, and limit to 160 characters (meta description limit)
   description=$(grep -m 1 '.' "$filepath" | sed 's/[\"\x27]//g' | cut -c1-160)
 
-  mkdir -p "$out_dir"
+  mkdir -p "$output_directory"
 
   body=$(pandoc "$filepath")
 
   # Replace {{TITLE}} in the Layout HTML template with the filename of the Markdown file being processed
   layout=$(echo "${HTML_LAYOUT//\{\{TITLE\}\}/$title}")
-  # Replace {{DESCRIPTION}} in the Layout HTML template with the first non-empty line of the Markdown file being processed
+  # Replace {{DESCRIPTION}} in the Layout HTML template with the first line of content in the Markdown file being processed
   layout=$(echo "${layout//\{\{DESCRIPTION\}\}/$description}")
   # Replace {{BODY}} in the Layout HTML template with the contents of the Markdown file being processed
   layout=$(echo "${layout//\{\{BODY\}\}/$body}")
   # Replace {{URL}} in the Layout HTML template with the URL of the Markdown file being processed
   layout=$(echo "${layout//\{\{URL\}\}/$url}")
 
-  echo "$layout" > "$out_path"
+  echo "$layout" > "$output_path"
 
   if [ "$PRETTIER_ENABLED" = true ]; then
-    prettier --write "$out_path"
+    prettier --write "$output_path"
   fi
 
-  echo "🔨🤠 Generated HTML: $out_path"
+  echo "🔨🤠 GENERATED: $output_path"
 done
